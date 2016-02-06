60685fa4b6b0e9308adc0d627208be06b26fbf80
ljhzh <469874564@qq.com>
2016-02-06 23:52:46 +0800

    Only a baseDao built for other Dao factory to extend package

---
+++ WIKI/src/main/java/Dao/DaoImpl/BaseDaoImpl.java
@@ -0,0 +1,54 @@
+package Dao.DaoImpl;
+
+import java.util.List;
+
+import Dao.BaseDao;
+
+public class BaseDaoImpl implements BaseDao {
+
+	public void save(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void saveAndUpdate(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object get(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Object> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Object> downlist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Object> uplist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getClassId(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getClassString(Object o, Integer index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
